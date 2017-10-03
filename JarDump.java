cfd3f5db1f6a3e861699179be150c415c9ed4db1
fuho <ondrej.dolejsi@gmail.com>
2017-10-03 16:58:28 -0400

    Dump all params more better

--- JarDump.java
+++ JarDump.java
@@ -1,8 +1,10 @@
 class JarDump {
     public static void main(String[] args) {
-          System.out.println("Command-line arguments:");
+          System.out.println("[Command-line arguments - one per line]");
           for (String arg : args) {
                 System.out.println(arg);
           }
+          System.out.println("[End of Command-line - one per line]");
+          System.exit(0);
     }
 }
